86a42c38205cf016963c0430c98d47764f926d05
mavrick w <mavwatts@gmail.com>
2020-04-11 02:28:21 -0400

    Updates

--- README.md
+++
@@ -1,9 +0,0 @@
-This project utilized the following technologies
--node
--react
--css
--javascript
--html
-
-reference api site 
-omdbapi.com
---
+++ precache-manifest.0e48f6e2771ffa1b73329f59c666035b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3162cdabd53399ca02cc067bc277d24",
+    "url": "/React-Movie-Database/index.html"
+  },
+  {
+    "revision": "3bc9b2f44350761949ed",
+    "url": "/React-Movie-Database/static/css/main.2da39c93.chunk.css"
+  },
+  {
+    "revision": "5ed37b472ee8a9104410",
+    "url": "/React-Movie-Database/static/js/2.403e5aea.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Movie-Database/static/js/2.403e5aea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3bc9b2f44350761949ed",
+    "url": "/React-Movie-Database/static/js/main.22183134.chunk.js"
+  },
+  {
+    "revision": "e08a98bf0127a8337d75",
+    "url": "/React-Movie-Database/static/js/runtime-main.2539bb77.js"
+  }
+]);
